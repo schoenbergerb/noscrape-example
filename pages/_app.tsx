cba966ef0961654284173e2b51cb39dafafddc67
bernhard <schoenbergerb@mail.de>
2021-12-17 15:27:52 +0100

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,23 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <div>
+    <Head>
+          <link
+            rel="preload"
+            href="/fonts/example/example.ttf"
+            as="font"
+            crossOrigin=""
+        />
+    </Head>
+    <body>
+      <Component {...pageProps} />
+    </body>
+  </div>
+  
+  
 }
 
 export default MyApp
