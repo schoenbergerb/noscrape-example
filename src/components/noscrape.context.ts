ce73bf0a56baa9f51ebb5906d915850a1f426f2e
Bernhard <mail@schoenberger.be>
2024-01-01 22:42:09 +0100

    remove obsolete files

--- src/components/noscrape.context.ts
+++
@@ -1,6 +0,0 @@
-import {EncryptionCharacterRange, Noscrape} from "@noscrape/noscrape";
-import serverContext from "server-only-context";
-
-export const [useNoscrape, setNoscrape] = serverContext(new Noscrape('public/example.ttf', {
-    characterRange: EncryptionCharacterRange.HIRAGANA
-}))
