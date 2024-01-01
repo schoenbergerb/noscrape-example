f408892a7eaa01436b896ab4dad82ea8a4f495e1
Bernhard <mail@schoenberger.be>
2024-01-01 22:42:20 +0100

    add headline component

---
+++ src/components/Headline.tsx
@@ -0,0 +1,11 @@
+import Obfuscated from "@/components/noscrape/Obfuscated";
+
+const Headline = () => {
+  return (
+    <h1 className="text-xl md:text-2xl lg:text-4xl">
+      <Obfuscated label="Welcome to Noscrape" />
+    </h1>
+  );
+};
+
+export default Headline;
